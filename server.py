b344f5316e0bc7f7432b500637f5a2b0e69b4f6e
Sanskar <sansyrox@gmail.com>
2018-10-06 23:33:39 +0530

    Fixed the patch one

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import pafy
 import vlc
 
 app = Flask(__name__)
-Instance = vlc.Instance()
+Instance = vlc.Instance('--no-video')
 player = Instance.media_player_new()
 url = ''
 
@@ -20,7 +20,8 @@ def youtube():
     vid = request.args.get('vid')
     url = 'https://www.youtube.com/watch?v=' + vid
     video = pafy.new(url)
-    best = video.getbest()
+    streams = video.audiostreams
+    best = streams[3]
     playurl = best.url
     Media = Instance.media_new(playurl)
     Media.get_mrl()
@@ -56,7 +57,7 @@ def restart():
     resp.status_code = 200
     return resp
 
-@app.route('/play')
+@app.route('/resume')
 def play():
     player.play()
     display_message = {"song":"played"}
@@ -65,4 +66,4 @@ def play():
     return resp
 
 if __name__ == '__main__':
-    app.run(debug=False,port=7070)
+    app.run(debug=True,port=7070,host= '0.0.0.0')
