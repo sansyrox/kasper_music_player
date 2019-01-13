17a6cf73cfc6bd1c2131ea0b9974456f7e2b40d2
Sanskar <sansyrox@gmail.com>
2019-01-14 00:57:06 +0530

    Bug Fixes in Front End

--- youtube_mp3_frontend/src/components/sidemenu/sidemenu.js
+++ youtube_mp3_frontend/src/components/sidemenu/sidemenu.js
@@ -9,7 +9,7 @@ class SideMenu extends Component {
   render(){
     return (
       <ul className='side-menu-container'>
-        <div class="tc pa4">
+        <div className="tc pa4">
           <img src="http://chittagongit.com//images/m-icon/m-icon-7.jpg" className="br-100 pa1 ba b--black-10 h3 w3" alt="avatar"/>
         </div>
         <li onClick={ handleClick } className={this.props.title === 'Browse' ? 'active side-menu-item hover-bg-blue pa1 bb b-white bb b-white': 'side-menu-item hover-bg-blue pa1 bb b-white'}>Browse</li>
